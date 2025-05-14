b5c1e777e7d12bf60e883520fe069327c67f6d2f
Sergio Eslava <seslavav@alumnos.unex.es>
2025-05-14 11:51:12 +0200

    project setup

---
+++ components/p3bot-bridge/generated/genericworker.h
@@ -0,0 +1,91 @@
+/*
+ *    Copyright (C) 2025 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef GENERICWORKER_H
+#define GENERICWORKER_H
+
+#include <stdint.h>
+#include <grafcetStep/GRAFCETStep.h>
+#include <ConfigLoader/ConfigLoader.h>
+#include <QStateMachine>
+#include <QEvent>
+#include <QString>
+#include <functional>
+#include <atomic>
+#include <QtCore>
+#include <variant>
+#include <unordered_map>
+
+
+#include <GenericBase.h>
+#include <OmniRobot.h>
+
+#define BASIC_PERIOD 100
+
+using TuplePrx = std::tuple<>;
+
+
+class GenericWorker : public QObject
+{
+Q_OBJECT
+public:
+	GenericWorker(const ConfigLoader& configLoader, TuplePrx tprx);
+	virtual ~GenericWorker();
+	virtual void killYourSelf();
+
+	void setPeriod(const std::string& state, int period);
+	int getPeriod(const std::string& state);
+
+	QStateMachine statemachine;
+	QTimer hibernationChecker;
+	std::atomic_bool hibernation = false;
+
+
+
+	virtual void OmniRobot_correctOdometer(int x, int z, float alpha) = 0;
+	virtual void OmniRobot_getBasePose(int &x, int &z, float &alpha) = 0;
+	virtual void OmniRobot_getBaseState(RoboCompGenericBase::TBaseState &state) = 0;
+	virtual void OmniRobot_resetOdometer() = 0;
+	virtual void OmniRobot_setOdometer(RoboCompGenericBase::TBaseState state) = 0;
+	virtual void OmniRobot_setOdometerPose(int x, int z, float alpha) = 0;
+	virtual void OmniRobot_setSpeedBase(float advx, float advz, float rot) = 0;
+	virtual void OmniRobot_stopBase() = 0;
+
+protected:
+	std::unordered_map<std::string, std::unique_ptr<GRAFCETStep>> states;
+	ConfigLoader configLoader;
+
+
+
+
+private:
+
+public slots:
+	virtual void initialize() = 0;
+	virtual void compute() = 0;
+	virtual void emergency() = 0;
+	virtual void restore() = 0;
+	void hibernationCheck();
+	
+signals:
+	void kill();
+	void goToEmergency();
+	void goToRestore();
+};
+
+#endif
